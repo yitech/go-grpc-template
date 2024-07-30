3620fcee9d0d774b9c9bd0d9cf642156c4cba3b0
yite <yite@icloud.com>
2024-07-30 18:20:25 +0800

    lint

--- api/v1/helloworld.go
+++ api/v1/helloworld.go
@@ -6,10 +6,12 @@ import (
 	pb "github.com/yitech/go-grpc-template/grpc/v1/helloworld"
 )
 
-type Greeter struct {
+// HelloWorld implements the GreeterServer interface.
+type HelloWorld struct {
 	pb.UnimplementedGreeterServer
 }
 
-func (s *Greeter) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+// SayHello sends a greeting.
+func (s *HelloWorld) SayHello(_ context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
